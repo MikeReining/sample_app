fcd04c81bd8707ac6ebb7b76f097b50217cb8aa6
grasshopperapps <emailmike@gmail.com>
2011-08-31 22:25:52 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4126305936182722523754e365238acbafb3b8150a0d1f85ca9210f31a1dfc3899193cd376c1f6c078f1b4f92d7ac924268cd30d4e58c7ccdd0b6ca2269eb44c'
